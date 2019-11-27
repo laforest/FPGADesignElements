ec5a09a27cb98167db306c5a9f8d319bac2e16c5
Charles Eric LaForest <eric.laforest@gmail.com>
2019-11-27 13:48:04 -0500

    Update Demultiplexer with BROADCAST parameter

--- Demultiplexer_Binary.v
+++ Demultiplexer_Binary.v
@@ -3,17 +3,37 @@
 
 // Connects the `word_in` input port to one of the words in the `words_out`
 // output port, as selected by the `output_port_selector` binary address, and
-// raises the corresponding valid bit in the `valids_out` output port.
+// raises the corresponding valid bit in the `valids_out` output port.  If the
+// `output_port_selector` value is greater than the number of output words
+// specified by the `OUTPUT_COUNT` parameter, then none of the `valids_out`
+// bits will be set.
 
-// All unselected output ports and valid bits are set to zero, and if the
-// `output_port_selector` value is greater than the number of output words,
-// then *all* output words and valid bits stay at zero.  This costs some logic,
-// but makes tracing a simulation easier, and adds some design security and
-// robustness since unselected downstream logic cannot snoop or accidentally
-// receive other data.
+//## Implementation Options
+
+// Set the `BROADCAST` parameter to 1 to simply replicate and connect
+// `word_in` to each word in `words_out`, without any logic. The valid bit
+// will indicate which downstream logic should accept the data, and other
+// logic can snoop the data if that's part of your larger design.
+
+// Set the `BROADCAST` parameter to 0 to send `word_in` to *only* the selected
+// word in `words_out`, whose valid bit is set. All other output words are
+// annulled to zero. If no valid bit is set, all output words stay at zero.
+
+// Not broadcasting the input to the output costs some logic, but less than it
+// appears from a standalone synthesis of the demultiplexer: the inferred
+// [Annullers](./Annuller.html) will very likely disappear into downstream LUT
+// logic, it also makes tracing a simulation easier, and adds some design
+// security and robustness since unselected downstream logic cannot snoop or
+// accidentally receive other data.
+
+// Setting `BROADCAST` to any value other than 1 or 0 will disconnect
+// `word_in` from `words_out`, raise some critical warnings in your CAD tool,
+// and generally cause a lot of downstream logic to optimize away, so you
+// should notice...
 
 module Demultiplexer_Binary
 #(
+    parameter       BROADCAST           = 0,
     parameter       WORD_WIDTH          = 0,
     parameter       ADDR_WIDTH          = 0,
     parameter       OUTPUT_COUNT        = 0,
@@ -42,24 +62,33 @@ module Demultiplexer_Binary
         .one_hot_out    (valids_out)
     );
 
-// Then, for each output port, annul the output if its valid bit is not set.
-// Thus, only the selected output port will have the `word_in` data. All
-// others will stay at zero.
+// If we are *not* broadcasting, then for each output port, annul the output
+// if its valid bit is not set.  Thus, only the selected output port will have
+// the `word_in` data. All others will stay at zero.  Otherwise, simply
+// replicate and connect the input to all outputs. [This is one of the very
+// rare cases where you must use the `assign`
+// statement.](./verilog.html#assign)
 
     generate
-        genvar i;
-        for (i=0; i < OUTPUT_COUNT; i=i+1) begin: per_output
-            Annuller
-            #(
-                .WORD_WIDTH     (WORD_WIDTH),
-                .IMPLEMENTATION ("AND")
-            )
-            output_gate
-            (
-                .annul          (valids_out[i] == 1'b0),
-                .data_in        (word_in),
-                .data_out       (words_out[WORD_WIDTH*i +: WORD_WIDTH])
-            );
+        if (BROADCAST == 0) begin
+            genvar i;
+            for (i=0; i < OUTPUT_COUNT; i=i+1) begin: per_output
+                Annuller
+                #(
+                    .WORD_WIDTH     (WORD_WIDTH),
+                    .IMPLEMENTATION ("AND")
+                )
+                output_gate
+                (
+                    .annul          ((valids_out[i] == 1'b0)),
+                    .data_in        (word_in),
+                    .data_out       (words_out[WORD_WIDTH*i +: WORD_WIDTH])
+                );
+            end
+        end
+        else
+        if (BROADCAST == 1) begin
+            assign words_out = {OUTPUT_COUNT{word_in}};
         end
     endgenerate
 
