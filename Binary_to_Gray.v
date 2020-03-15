c0a6afb99375dcbee3c168f6f38d4a0e727bb263
Jeffrey Cassidy <jeffrey.cassidy@gmail.com>
2020-03-15 17:00:31 -0400

    Response to Eric's comments: fixed Verilator warnings, added initial blocks, added comments

--- Binary_to_Gray.v
+++ Binary_to_Gray.v
@@ -1,30 +1,41 @@
-`default_nettype none
+// Converts from an unsigned binary number to Gray code (Reflected Binary Code)
+// see en.wikipedia.org/wiki/Gray_code
+//
+// See documentation in Gray_to_Binary.v
 
-/** Converts from an unsigned binary number to Gray code (Reflected Binary Code)
- * see en.wikipedia.org/wiki/Gray_code
- */
+`default_nettype none
 
 module Binary_to_Gray
 #(
-    parameter WIDTH
+    parameter integer WIDTH=1
 )
 (
     input  wire [WIDTH-1:0]  binary_in,
-    output wire [WIDTH-1:0]  gray_out
+    output reg  [WIDTH-1:0]  gray_out
 );
+
+    localparam ZERO = {WIDTH{1'b0}};
 
     function automatic [WIDTH-1:0] binary_to_gray(input [WIDTH-1:0] binary);
         integer i;
         reg [WIDTH-1:0] gray;
         begin
-            for(i=0;i<WIDTH-1;i=i+1)
+            for(i=0;i<WIDTH-1;i=i+1) begin
                 gray[i] = binary[i] ^ binary[i+1];
+            end
             gray[WIDTH-1] = binary[WIDTH-1];
 
             binary_to_gray = gray;
         end
     endfunction
 
-    assign gray_out = binary_to_gray(binary_in);
+    initial begin
+        gray_out = ZERO;
+    end
+
+    
+    always@(*) begin
+        gray_out = binary_to_gray(binary_in);
+    end
 
 endmodule
--- Counter_Gray_Tb.py
+++ Counter_Gray_Tb.py
@@ -11,13 +11,15 @@ lib = vu.add_library('lib')
 for file_name in [
     'Gray_to_Binary.v',
     'Binary_to_Gray.v',
+    'Gray.sv'
     ]:
 
     src_file = lib.add_source_file(os.path.join(source_dir,file_name))
     src_file.set_compile_option('modelsim.vlog_flags',['-vlog01compat'])
 
 for file_name in [
-    'Counter_Gray_Tb.sv'
+    'Counter_Gray_Tb.sv',
+    'Counter_Gray_SV_Tb.sv'
     ]:
     lib.add_source_file(os.path.join(source_dir,file_name))
 
@@ -33,5 +35,16 @@ for test in counter_gray_tb.get_tests('*'):
                 'WIDTH':width,
                 'PRINT':1
             })
+
+counter_gray_sv_tb = lib.test_bench('counter_gray_sv_tb')
+
+for test in counter_gray_sv_tb.get_tests('*'):
+    for width in [4,5,6]:
+        test.add_config(
+            name="W%d"%(width),
+            generics={
+                'WIDTH':width
+            })
+
 
 vu.main()
