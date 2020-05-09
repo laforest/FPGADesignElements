0a594a10eddc702af9f12c3c92baea62ca0454e6
Charles Eric LaForest <eric.laforest@gmail.com>
2020-05-09 12:39:55 -0400

    Finished Debouncer

--- Debouncer_Low_Latency.v
+++ Debouncer_Low_Latency.v
@@ -12,10 +12,8 @@
 // * For discussion and some experimental data on switch bounce, see Jack
 // Ganssle's ["A Guide to Debouncing, or, How to Debounce a Contact in Two
 // Easy Pages"](http://www.ganssle.com/debouncing.htm).
-
 // * Horowitz' and Hill's *"The Art of Electronics"* also has some information
 // on signal integrity, measurement, and debouncing scattered throughout.
-
 // * Finally, Dan Gissequist's ["How to eliminate button bounces with digital
 // logic"](https://zipcpu.com/blog/2017/08/04/debouncing.html), goes over some
 // pitfalls and arrives to a similar design as shown here.
@@ -63,82 +61,90 @@
 // with a very uneven duty cycle (e.g.: rapid short pulses), so a separate
 // wait time for both rising and falling transitions can be set.
 
-// FIXME add notes and discussion on calibration and check of correct
-// operation
+// To calibrate the delay, observe the `diag_synchronized_input` and
+// `diag_ignoring_input` signals on a logic analyzer or scope while you
+// operate the switch. The first will show you the behaviour of the input
+// bounce and its duration, and if your switch is suitable for your
+// application. The second will show you if the Debouncer is ignoring
+// the input bounce long enough for reliable operation, and if valid inputs
+// are being lost (e.g.: multiple valid switch events inside a single delay).
+
+// *Note that if the input reliably returns to the previous state before the
+// delay is done, the `input_clean` will not reflect the change until the
+// delay is done.* This does put a rate limit on the input.
 
 `default_nettype none
 
 module Debouncer_Low_Latency
 #(
-    parameter WORD_WIDTH        = 22,    // Wide enough to hold largest delay.
+    parameter COUNTER_WIDTH     = 32,   // Wide enough to hold largest delay.
     parameter INITIAL_INPUT     = 1'b0, // 1'b0 or 1'b1. The input rest state.
     parameter EXTRA_CDC_STAGES  = 0     // Must be 0 or greater.
 )
 (
     input   wire                        clock,
-    input   wire                        clear,
-    input   wire                        enable,
+    // No reset or enable, as that could cause artificial input events.
 
-    input   wire    [WORD_WIDTH-1:0]    delay_cycles_rising,
-    input   wire    [WORD_WIDTH-1:0]    delay_cycles_falling,
+    input   wire    [COUNTER_WIDTH-1:0] delay_cycles_rising,
+    input   wire    [COUNTER_WIDTH-1:0] delay_cycles_falling,
 
     input   wire                        input_raw,
-    output  reg                         input_falling,
-    output  reg                         input_rising,
+    output  wire                        input_falling,
+    output  wire                        input_rising,
     output  wire                        input_clean,
 
-    // For calibration and testing only
-    output  reg                         diag_captured_input,
+    // For calibration and testing (see notes above)
     output  reg                         diag_synchronized_input,
     output  reg                         diag_ignoring_input
 );
 
     initial begin
-        input_falling           = 1'b0;
-        input_rising            = 1'b0;
-        diag_captured_input     = INITIAL_INPUT;
         diag_synchronized_input = INITIAL_INPUT;
         diag_ignoring_input     = 1'b0;
     end
 
 // Let's generate counter values of the correct width.
 
-    localparam  COUNTER_ONE   = {{WORD_WIDTH-1{1'b0}},1'b1};
-    localparam  COUNTER_ZERO  = {WORD_WIDTH{1'b0}};
+    localparam  COUNTER_ONE   = {{COUNTER_WIDTH-1{1'b0}},1'b1};
+    localparam  COUNTER_ZERO  = {COUNTER_WIDTH{1'b0}};
 
-// First, capture the input in an I/O register.  We expect the CAD tool to do
-// this for us, but let's provide attributes to ask explicitly.  This will
-// filter out *some* glitches and marginal input levels too, as well as
-// provide a way to enable the input.
+// First, capture the input into an I/O register to filter out glitches and
+// random sub-threshold analog noise between clock edges, and to convert slow
+// input level changes (due to input capacitance and resistance) into
+// a single, sharper edge (or at least a short train of cleaner pulses). All
+// of this cleanup will make the CDC Synchronizer's job easier and more
+// reliable.
 
-    wire captured_input;
+// We also give the CAD tool some attributes to place this register into an
+// I/O register if possible. This is useful if this Debouncer is fed by
+// a regional I/O clock, and to make sure we don't let analog noise into the
+// logic fabric. It shouldn't matter, but in extreme cases the noise could
+// couple to other logic lines. Analog noise is outside the expected behaviour
+// of signals inside the FPGA, so let's avoid it.
+
+// *NOTE: The output of this register will still end up briefly metastable or
+// otherwise marginal sometimes, and must be properly synchronized to the
+// clock later on.*
+
+// We don't use a [Register](./Register.html) module here since we can't apply
+// the necessary I/O attributes to a module, and we don't want to have any
+// enable or reset logic on this register, which could cause false input
+// events.
 
     (* IOB = "true" *)  // Vivado
     (* useioff = 1 *)   // Quartus
 
-    Register
-    #(
-        .WORD_WIDTH     (1),
-        .RESET_VALUE    (INITIAL_INPUT)
-    )
-    input_capture
-    (
-        .clock          (clock),
-        .clock_enable   (enable),
-        .clear          (clear),
-        .data_in        (input_raw),
-        .data_out       (captured_input)
-    );
+    reg captured_input = INITIAL_INPUT;
 
-    always @(*) begin
-        diag_captured_input = captured_input;
+    always @(posedge clock) begin
+        captured_input <= input_raw;
     end
 
-// Then, synchronize the captured input to the clock, noise and all.
-// This will filter out marginal inputs causing metastability, and glitches
-// will either get dropped, or converted to proper 1-cycle pulses.
-// Add stages if necessary (likely if you have *very* slow level transitions).
-// From here on, the switch noise is a clean digital signal we can process.
+// Then, we synchronize the captured, de-noised input to the clock to filter
+// out any remaining marginal or metastable signals, which will either get
+// dropped, or converted to proper synchronous logic pulses.  Add stages if
+// necessary, likely for very high clock speeds.  From here on, the input is
+// a clean digital signal we can debounce.
 
     wire synchronized_input;
 
@@ -163,18 +169,20 @@ module Debouncer_Low_Latency
 // first observed edge can only signify a change in switch state and we can
 // report it immediately.
 
-// The rest of the switching time, with its noise, is not important. We only
-// need to wait it out in parallel, by loading the counter with a non-zero
-// value at that instant and ignoring the input while the counter counts down
-// to zero.
+// The rest of the switching time, with its bouncing transitions, is not
+// important. We only need to wait it out in parallel by loading the counter
+// with a non-zero value at that instant and ignoring the input while the
+// counter counts down to zero.
 
-    reg counter_load = 1'b0;
-    reg counter_run  = 1'b0;
+    reg input_rising_internal   = 1'b0;
+    reg input_falling_internal  = 1'b0;
+    reg counter_load            = 1'b0;
+    reg counter_run             = 1'b0;
 
     always @(*) begin
-        input_rising    = (synchronized_input == 1'b1) && (input_clean   == 1'b0) && (counter_run == 1'b0);
-        input_falling   = (synchronized_input == 1'b0) && (input_clean   == 1'b1) && (counter_run == 1'b0);
-        counter_load    = (input_rising       == 1'b1) || (input_falling == 1'b1);
+        input_rising_internal   = (synchronized_input       == 1'b1) && (input_clean            == 1'b0) && (counter_run == 1'b0);
+        input_falling_internal  = (synchronized_input       == 1'b0) && (input_clean            == 1'b1) && (counter_run == 1'b0);
+        counter_load            = (input_rising_internal    == 1'b1) || (input_falling_internal == 1'b1);
     end
 
     always @(*) begin
@@ -182,7 +190,7 @@ module Debouncer_Low_Latency
     end
 
 // Capture the gated edge detection pulses in a latch, where each type of
-// pulse sets the latch to mirror the state the switch is entering, based on
+// pulse sets the latch to mirror the state the input is entering, based on
 // the first detected edge.
 
     Pulse_Latch
@@ -192,43 +200,74 @@ module Debouncer_Low_Latency
     input_mirror
     (
         .clock          (clock),
-        .clear          (input_falling),
-        .pulse_in       (input_rising),
+        .clear          (input_falling_internal),
+        .pulse_in       (input_rising_internal),
         .level_out      (input_clean)
     );
 
-// Finally, when a rising or falling edge is detected, load (and start) the delay counter,
-// which disables edge detection while running down to zero. A different value
-// may be loaded for rising and falling edges.
+// And register the rising/falling pulses so they arrive in sync with
+// `input_clean`.
 
-    wire [WORD_WIDTH-1:0] delay_cycles;
+    Register
+    #(
+        .WORD_WIDTH     (1),
+        .RESET_VALUE    (1'b0)
+    )
+    sync_input_rising
+    (
+        .clock          (clock),
+        .clock_enable   (1'b1),
+        .clear          (1'b0),
+        .data_in        (input_rising_internal),
+        .data_out       (input_rising)
+    );
+
+    Register
+    #(
+        .WORD_WIDTH     (1),
+        .RESET_VALUE    (1'b0)
+    )
+    sync_input_falling
+    (
+        .clock          (clock),
+        .clock_enable   (1'b1),
+        .clear          (1'b0),
+        .data_in        (input_falling_internal),
+        .data_out       (input_falling)
+    );
+
+// Finally, when a rising or falling edge is detected, load (and start) the
+// delay counter, which disables edge detection while running down to zero.
+// A different value may be loaded for rising and falling edges.
+
+    wire [COUNTER_WIDTH-1:0] delay_cycles;
 
     Multiplexer_One_Hot
     #(
-        .WORD_WIDTH     (WORD_WIDTH),
+        .WORD_WIDTH     (COUNTER_WIDTH),
         .WORD_COUNT     (2),
         .OPERATION      ("OR"),
         .IMPLEMENTATION ("AND")
     )
     delay_select
     (
-        .selectors      ({input_falling,        input_rising}),
-        .words_in       ({delay_cycles_falling, delay_cycles_rising}),
+        .selectors      ({input_falling_internal, input_rising_internal}),
+        .words_in       ({delay_cycles_falling,   delay_cycles_rising}),
         .word_out       (delay_cycles)
     );
 
-    wire [WORD_WIDTH-1:0] count;
+    wire [COUNTER_WIDTH-1:0] count;
 
     Counter_Binary
     #(
-        .WORD_WIDTH     (WORD_WIDTH),
+        .WORD_WIDTH     (COUNTER_WIDTH),
         .INCREMENT      (COUNTER_ONE),
         .INITIAL_COUNT  (COUNTER_ZERO)
     )
     delay_counter
     (
         .clock          (clock),
-        .clear          (clear),
+        .clear          (1'b0),
         .up_down        (1'b1), // 0/1 --> up/down
         .run            (counter_run),
         .load           (counter_load),
