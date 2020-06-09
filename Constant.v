50028a531cf3410b55e416308a10d30a74726651
Charles Eric LaForest <eric.laforest@gmail.com>
2020-06-08 23:04:50 -0400

    Remove one case where assign must be used. Update Constant to match.
    This should have been obvious from the start, but at least it uncovered
    a corner case.

--- Constant.v
+++ Constant.v
@@ -1,26 +1,25 @@
 
 //# A Constant Value Output
-//
+
 // Normally this is merely a `localparam` inside another module, but it may
 // be useful implemented as a module on its own to work with other IP systems
 // such as Xilinx's IPI (IP Integrator), where you may need to feed an IP
 // block a constant value.
 
-// This is also a [rare corner case where `assign` must be
-// used](./verilog.html#assign).
-
 `default_nettype none
 
 module Constant
 #(
-    parameter WORD_WIDTH    = 0,
-    parameter VALUE         = 0
+    parameter                   WORD_WIDTH  = 0,
+    parameter  [WORD_WIDTH-1:0] VALUE       = 0
 )
 (
-    output wire [WORD_WIDTH-1:0] constant_out
+    output reg [WORD_WIDTH-1:0] constant_out
 );
 
-    assign constant_out = VALUE;
+    initial begin
+        constant_out = VALUE;
+    end
 
 endmodule
 
