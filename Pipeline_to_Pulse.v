19374d18722a97815de7c80255b4000b84135512
Charles Eric LaForest <eric.laforest@gmail.com>
2020-07-04 21:33:30 -0400

    Split Pipeline/Pulse interfaces. Some inputs have no ouputs, so this is needed.

--- Pipeline_to_Pulse.v
+++ Pipeline_to_Pulse.v
@@ -1,44 +1,31 @@
 
 //# Pipeline to Pulse Interface
 
-// Converts a receiving ready/valid pipeline handshake into an input pulse
-// interface to a connected module, and then converts that module's output
-// pulse interface into a sending ready/valid pipeline handshake. The input
-// and output pipeline handshakes are coupled so input data is accepted no
-// faster than output data is generated and read out, and overlapping input
-// and output is supported.
-// *The path from the input pulse interface to the output pulse interface must
-// have at least one pipeline stage. No combinational paths are allowed else
-// the input and output ready/valid handshake logic will form a loop.*
+// Wraps a module with a pulse input interface inside a ready/valid input
+// handshake interface. Supports full throughput (one input per cycle) if
+// necessary, though that's not usually the case when this interface is
+// needed.
 
-//## Dealing with Irreducible Latency
+// *The connected module must have at least one pipeline stage from input to
+// output. No combinational paths allowed else the input and output handshake
+// logic will form a loop.*
 
-// Sometimes a module will have some irreducible and possibly variable
-// latency, where it takes multiple clock cycles between an updated input and
-// the resulting updated output, *and you cannot update the input again until
-// the output has updated*.  In other words, it has an initiation interval
-// greater than 1.
+// When we have a module that cannot be fully pipelined due to a data
+// dependency (e.g.: because of a backwards loop in the pipeline, or simply by
+// the iterative nature of the implemented algorithm), and thus cannot accept
+// a new input every cycle (i.e.: it has an initiation interval greater than
+// 1), then we design the connected module to accept a new input with
+// a one-cycle valid pulse
 
-// Lets constrain the interface of such a module to accept a new input with
-// a one-cycle valid pulse, and to signal the updated output, 1 or more cycles
-// later, with a corresponding one-cycle valid output pulse, and allow that we
-// can simultaneously update the input and the output. *The updated output
-// must remain constant until the next one-cycle valid output pulse.*
+// This Pipeline to Pulse module converts a pipeline input with a ready/valid
+// handshake into a pulse input interface and prevents updating the input
+// faster than the connected module can handle, based on a separate signal
+// which indicates that new data can be accepted, usually from a similar
+// output handshake interface.
 
-// Given these design constraints, we can consistenly convert those input and
-// output pulse interfaces into input and output elastic pipeline interfaces
-// (ready/valid handshake) which prevent updating the input faster than the
-// output, maintain maximal throughput by overlapping input and output updates
-// if possible, and do not require any knowledge of the duration of the
-// module's irreducible latency.
-
-// Note that the module's updated output can only be read out *once* from the
-// elastic pipeline interface, even though it is held steady until the next
-// output update. This constraint is necessary to meet some Kahn Process
-// Network criteria (for future use), and to allow the module to use its
-// output registers as part of its computations rather than have extra buffers
-// to enable overlapping computation and communication. Instead, we provide
-// that buffering via a [Pipeline Skid Buffer](./Pipeline_Skid_Buffer.html).
+// We assume here that the connected module is not C-Slowed, though that is
+// allowed. You will have to keep track of the separate computation streams
+// yourself in the enclosing module.
 
 `default_nettype none
 
@@ -50,65 +37,52 @@ module Pipeline_to_Pulse
     input   wire                        clock,
     input   wire                        clear,
 
-    // Pipeline input and output
+    // Pipeline input
     input   wire                        valid_in,
     output  reg                         ready_in,
     input   wire    [WORD_WIDTH-1:0]    data_in,
 
-    output  wire                        valid_out,
-    input   wire                        ready_out,
-    output  wire    [WORD_WIDTH-1:0]    data_out,
-
-    // Pulse interface to connected module
-    // in -> module -> out
-    output  reg                         module_pulse_in,
+    // Pulse interface to connected module input
     output  reg     [WORD_WIDTH-1:0]    module_data_in,
-    input   wire                        module_pulse_out,
-    input   wire    [WORD_WIDTH-1:0]    module_data_out
+    output  reg                         module_data_in_valid,
+
+    // Signal that the module can accept the next input
+    input   wire                        module_ready
 );
 
     localparam WORD_ZERO = {WORD_WIDTH{1'b0}};
 
     initial begin
-        ready_in        = 1'b1; // matches logic below for simulation
-        module_data_in  = WORD_ZERO;
-        module_pulse_in = 1'b0;
+        ready_in                = 1'b1; // matches logic below for simulation
+        module_data_in          = WORD_ZERO;
+        module_data_in_valid    = 1'b0;
     end
 
-// Express the usual conditions to complete a ready/valid handshake. The
-// "internal" signals connect to a [Skid Buffer](./Pipeline_Skid_Buffer.html)
-// that deals with the final output handshake to the downstream pipeline.
+// Express the usual conditions to complete a ready/valid handshake. 
 
-    reg input_handshake_done  = 1'b0;
-    reg output_handshake_done = 1'b0;
-
-    wire ready_out_internal;
-    reg  valid_out_internal = 1'b0;
+    reg input_handshake_done = 1'b0;
 
     always @(*) begin
-        input_handshake_done    = (valid_in           == 1'b1) && (ready_in           == 1'b1);
-        output_handshake_done   = (valid_out_internal == 1'b1) && (ready_out_internal == 1'b1);
+        input_handshake_done = (valid_in == 1'b1) && (ready_in == 1'b1);
     end
-
-//## Input Handshake to Input Pulse
 
 // Input data goes straight into the connected module once the input handshake
 // is complete. The `input_handshake_done` signal will be interrupted and
 // become a single-cycle pulse by later logic.
 
     always @(*) begin
-        module_data_in  = data_in;
-        module_pulse_in = input_handshake_done;
+        module_data_in          = data_in;
+        module_data_in_valid    = (input_handshake_done == 1'b1); 
     end
 
-// We need to have `ready_in` be 1 initially and after a `clear`, else we
+// We need to have `ready_in` be 1 both initially and after a `clear`, else we
 // can't complete the initial input handshake (as the connected module has no
-// input readiness signal by design) and nothing would ever start.  This
-// initial state contradicts the later clearing of `ready_in_latched` back to
-// zero once the input handshake is done. So we instead keep that initial
-// state in a separate pulse latch. It starts cleared and will get set exactly
-// *once* when the initial input handshake completes, and stay will stay
-// constant until reset.
+// input readiness signal, by design) and nothing would ever start.  This
+// initial state contradicts the use of "clear" to bring `ready_in_latched`
+// back to zero once the input handshake is done. So we instead keep that
+// initial state in a separate pulse latch. It starts cleared and will get set
+// exactly *once* when the initial input handshake completes, and stay
+// constant until cleared.
 
     wire initial_ready_in;
 
@@ -124,14 +98,14 @@ module Pipeline_to_Pulse
         .level_out      (initial_ready_in)
     );
 
-// Now latch the value of `ready_in` which is set by completing the output
-// handshake, and cleared by completing the input handshake (or by the
-// top-level `clear` as reset).
+// Now latch the value of `ready_in` which is set by signalling the connected
+// module is ready, and cleared when completing the input handshake (or by
+// `clear`).
 
     reg clear_ready_in_latched = 1'b0;
 
     always @(*) begin
-        clear_ready_in_latched = (clear == 1'b1) || (input_handshake_done == 1'b1);
+        clear_ready_in_latched = (input_handshake_done == 1'b1) || (clear == 1'b1);
     end
 
     wire ready_in_latched;
@@ -144,69 +118,17 @@ module Pipeline_to_Pulse
     (
         .clock      (clock),
         .clear      (clear_ready_in_latched),
-        .pulse_in   (output_handshake_done),
+        .pulse_in   (module_ready),
         .level_out  (ready_in_latched)
     );
 
-// Use the initial `ready_in` state, pass the output handshake completion
-// directly to `ready_in` to remove a cycle of latency, and latch the
-// `ready_in` state if we don't finish an input handshake right away.
+// Use the initial state, pass `module_ready` to `ready_in` to remove a cycle
+// of latency, and latch the ready state if we don't finish an input handshake
+// right away.
 
     always @(*) begin
-        ready_in = (ready_in_latched == 1'b1) || (output_handshake_done == 1'b1) || (initial_ready_in == 1'b0);
+        ready_in = (initial_ready_in == 1'b0) || (ready_in_latched == 1'b1) || (module_ready == 1'b1);
     end
-
-//## Output Pulse to Output Handshake
-
-// Now handle the output ready/valid handshake, starting when the connected
-// module updates its output data to set `valid_out`, and completing the
-// output ready/valid handshake to clear it.
-
-    wire valid_out_latched;
-
-    Pulse_Latch
-    #(
-        .RESET_VALUE    (1'b0)
-    )
-    generate_valid_out_latched
-    (
-        .clock          (clock),
-        .clear          (output_handshake_done),
-        .pulse_in       (module_pulse_out),
-        .level_out      (valid_out_latched)
-    );
-
-// Pass the module output pulse directly, and also latch it, as
-// `valid_out_internal`. This removes a cycle of latency and allows the output
-// handshake to complete later.  We use "internal" signals, as the final Skid
-// Buffer will provide the final output handshake to the enclosing module.
-
-    always @(*) begin
-        valid_out_internal = (valid_out_latched == 1'b1) || (module_pulse_out == 1'b1);
-    end
-
-// Buffer the output handshake to cut the backwards combinational path from
-// `ready_out` to `ready_in`, to allow a transfer through the pulse-driven
-// module every cycle if it supports it, and to buffer the output data so the
-// next computation can begin immediately.
-
-    Pipeline_Skid_Buffer
-    #(
-        .WORD_WIDTH (WORD_WIDTH)
-    )
-    buffer_out
-    (
-        .clock          (clock),
-        .clear          (clear),
-
-        .input_valid    (valid_out_internal),
-        .input_ready    (ready_out_internal),
-        .input_data     (module_data_out),
-
-        .output_valid   (valid_out),
-        .output_ready   (ready_out),
-        .output_data    (data_out)
-    );
 
 endmodule
 
---
+++ Pulse_to_Pipeline.v
@@ -0,0 +1,129 @@
+
+//# Pulse to Pipeline Interface
+
+// Wraps a module with an output pulse interface inside a ready/valid output
+// handshake interface. Supports full throughput (one output per cycle) if
+// necessary, though that's not usually the case when this interface is
+// needed.
+
+// *The connected module must have at least one pipeline stage from input to
+// output. No combinational paths allowed else the input and output handshake
+// logic will form a loop.*
+
+// When we have a module that cannot be fully pipelined due to a data
+// dependency (e.g.: because of a backwards loop in the pipeline, or simply by
+// the iterative nature of the implemented algorithm), and thus cannot accept
+// a new input every cycle (i.e.: it has an initiation interval greater than
+// 1), then we design the connected module to accept a new input with
+// a one-cycle valid pulse, and to signal the updated output 1 or more cycles
+// later with a corresponding one-cycle output pulse. 
+
+// This Pulse to Pipeline module converts that output pulse interface into
+// a pipeline output with a ready/valid handshake, and once read out, signals
+// the corresponding input interface that new data can be accepted.
+
+// Note that the connected module result can only be read out once, even if
+// held steady until the next update. This is necessary to meet some Kahn
+// Process Network criteria, and to allow for the connected module to use its
+// output registers as part of its computations, rather than have extra
+// buffers (which we instead hold here as the Skid Buffer).
+
+// We assume here that the connected module is not C-Slowed, though that is
+// allowed. You will have to keep track of the separate computation streams
+// yourself in the enclosing module.
+
+`default_nettype none
+
+module Pulse_to_Pipeline
+#(
+    parameter WORD_WIDTH = 0
+)
+(
+    input   wire                        clock,
+    input   wire                        clear,
+
+    // Pipeline output
+    output  wire                        valid_out,
+    input   wire                        ready_out,
+    output  wire    [WORD_WIDTH-1:0]    data_out,
+
+    // Pulse interface from connected module
+    input   wire    [WORD_WIDTH-1:0]    module_data_out,
+    input   wire                        module_data_out_valid,
+
+    // Signal that the module can accept the next input
+    output  reg                         module_ready
+);
+
+    localparam WORD_ZERO = {WORD_WIDTH{1'b0}};
+
+    initial begin
+        module_ready = 1'b0;
+    end
+
+// Express the usual conditions to complete a ready/valid handshake.  The
+// "internal" signals connect to a Skid Buffer that deals with the final
+// output handshake to the downstream pipeline. When we have transferred the
+// module's output data to the Skid Buffer, we signal the module is ready for
+// the next input.
+
+    reg  valid_out_internal     = 1'b0;
+    wire ready_out_internal;
+    reg  output_handshake_done  = 1'b0;
+
+    always @(*) begin
+        output_handshake_done   = (valid_out_internal == 1'b1) && (ready_out_internal == 1'b1);
+        module_ready            = (output_handshake_done == 1'b1);
+    end
+
+// The output ready/valid handshake starts when the connected module updates
+// its output data. We use "internal" signals, as the final Skid Buffer will
+// provide the output handshake to the enclosing module.
+
+    wire valid_out_latched;
+
+    Pulse_Latch
+    #(
+        .RESET_VALUE    (1'b0)
+    )
+    generate_valid_out_latched
+    (
+        .clock          (clock),
+        .clear          (output_handshake_done),
+        .pulse_in       (module_data_out_valid),
+        .level_out      (valid_out_latched)
+    );
+
+// Pass the module pulse directly, and also latch it. This removes a cycle of
+// latency and still allows the output handshake to complete later if the
+// downstream logic is not ready.
+
+    always @(*) begin
+        valid_out_internal = (valid_out_latched == 1'b1) || (module_data_out_valid == 1'b1);
+    end
+
+// Buffer the output handshake to both cut the backwards combinational path
+// from `ready_out` to `module_ready`, and to allow a transfer through the
+// pulse-driven module every cycle, if possible, while supporting pipeline
+// back-pressure.
+
+    Pipeline_Skid_Buffer
+    #(
+        .WORD_WIDTH (WORD_WIDTH)
+    )
+    buffer_out
+    (
+        .clock          (clock),
+        .clear          (clear),
+
+        .input_valid    (valid_out_internal),
+        .input_ready    (ready_out_internal),
+        .input_data     (module_data_out),
+
+        .output_valid   (valid_out),
+        .output_ready   (ready_out),
+        .output_data    (data_out)
+    );
+
+endmodule
+
