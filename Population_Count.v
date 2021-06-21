6f325e308e653867fa6020cec8cac400b2fb3107
Charles Eric LaForest <eric.laforest@gmail.com>
2021-06-21 17:07:42 -0400

    Fix the Population Count to handle the case where only bits are counted, resulting in a zero-width PAD.

--- Population_Count.v
+++ Population_Count.v
@@ -87,14 +87,15 @@ module Population_Count
 
 // Then let's calculate how many pairs of bits we have to process, and the
 // zero-padding we will use to extend them to POPCOUNT_WIDTH, so all the
-// adders work on the same number of bits. Using the maximum number of
-// bits is a lot easier than figuring out the necessary adder bit width at
-// each step, and since the padding is constant zero, it wil be optimized
-// away during synthesis.
+// adders work on the same number of bits. Using the maximum number of bits is
+// a lot easier than figuring out the necessary adder bit width at each step,
+// and since the padding is constant zero, it wil be optimized away during
+// synthesis. If no padding is required, we return the otherwise impossible
+// maximal `PAD_WIDTH` for later special case handling.
 
     localparam PAIR_COUNT       = WORD_WIDTH / 2;
     localparam PAIR_WORD_WIDTH  = PAIR_COUNT * 2;
-    localparam PAD_WIDTH        = POPCOUNT_WIDTH - 2;
+    localparam PAD_WIDTH        = POPCOUNT_WIDTH > 2 ? POPCOUNT_WIDTH - 2 : POPCOUNT_WIDTH;
     localparam PAD              = {PAD_WIDTH{1'b0}};
 
 // Then define our working space: a vector of paircounts holding all bit
@@ -126,7 +127,15 @@ module Population_Count
 
     always @(*) begin
         paircount[0 +: 2]               = popcount2bits[word_in[0 +: 2]];
-        popcount[0 +: POPCOUNT_WIDTH]   = {PAD,paircount[0 +: 2]};
+        // This is decided at elaboration, but the linter doesn't know that.
+        if (PAD_WIDTH == POPCOUNT_WIDTH) begin
+            popcount[0 +: POPCOUNT_WIDTH]   = paircount[0 +: 2];
+        end 
+        else begin
+            // verilator lint_off WIDTH
+            popcount[0 +: POPCOUNT_WIDTH]   = {PAD,paircount[0 +: 2]};
+            // verilator lint_on  WIDTH
+        end
 
 // Now repeat for all remaining bit pairs, but also accumulate the popcount
 // from the previous iteration.  Note the start index due to the peeled-out
@@ -134,7 +143,15 @@ module Population_Count
 
         for(i=1; i < PAIR_COUNT; i=i+1) begin : per_paircount
             paircount[2*i +: 2]                          = popcount2bits[word_in[2*i +: 2]];
-            popcount[POPCOUNT_WIDTH*i +: POPCOUNT_WIDTH] = {PAD,paircount[2*i +: 2]} + popcount[POPCOUNT_WIDTH*(i-1) +: POPCOUNT_WIDTH];
+            // This is decided at elaboration, but the linter doesn't know that.
+            if (PAD_WIDTH == POPCOUNT_WIDTH) begin
+                popcount[POPCOUNT_WIDTH*i +: POPCOUNT_WIDTH] = paircount[2*i +: 2] + popcount[POPCOUNT_WIDTH*(i-1) +: POPCOUNT_WIDTH];
+            end 
+            else begin
+                // verilator lint_off WIDTH
+                popcount[POPCOUNT_WIDTH*i +: POPCOUNT_WIDTH] = {PAD,paircount[2*i +: 2]} + popcount[POPCOUNT_WIDTH*(i-1) +: POPCOUNT_WIDTH];
+                // verilator lint_on  WIDTH
+            end
         end
 
 // If the input word width was odd, pad up the last bit not in a pair to the
